3f5b7692880cab68ca4964446775ed15b5dc4644
Vinicius Moraes <vsantana0212@gmail.com>
2017-11-23 17:41:15 -0300

    Adicionando validação de email e máscara de telefone.

--- AgendaRails/app/models/contato.rb
+++ AgendaRails/app/models/contato.rb
@@ -2,4 +2,5 @@ class Contato < ApplicationRecord
   validates :nome, presence: true, length: {minimum: 5}
   validates :endereco, presence: true
   validates :telefone, length: {minumum: 10, maximum: 11}
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
